2b913265d5f55f317c4619aeb1baad91f1f844d3
acer <ssempuumamosesm@gmail.com>
2020-02-18 13:04:47 +0300

    hello world

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,7 +27,28 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent); message.setText(""); }
 
 
+        public boolean onCreateOptionsMenu(Menu menu){
+            MenuInflater inflater=getMenuInflater();
+            inflater.inflate(R.menu.mymenu,menu);
 
+            return true;
+        }
+        public boolean onOPtionsItemSelected(MenuItem item){
+
+        switch (item.getItemId()){
+
+            case R.id.showvideo:
+                startActivity(new Intent(this,Aspire.class));
+            return true;
+
+            case R.id.showaudio:
+                startActivity(new Intent(this,MainActivity.class));
+                return true;
+
+                default:
+                    return super.onContextItemSelected(item);
+        }
+        }
 
 
 
