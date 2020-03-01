c0649082154554edffee1d9cdaf6f9aada71873b
acer <ssempuumamosesm@gmail.com>
2020-03-01 22:57:54 +0300

    hello world

--- app/src/main/java/com/example/androidproject/SplashActivity.java
+++ app/src/main/java/com/example/androidproject/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },3000);
+        },1000);
 
     }
 }
