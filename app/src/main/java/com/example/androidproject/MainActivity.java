dcff6fd8627e136df325e81d6d5e6a48d72b93fe
acer <ssempuumamosesm@gmail.com>
2020-04-11 14:29:27 +0300

    hello world

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
 
 
         registerReceiver(myreciever,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
