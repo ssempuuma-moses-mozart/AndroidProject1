d1aa0f8197d02d618bf713e69fe3252a0d75b32a
acer <ssempuumamosesm@gmail.com>
2020-12-09 08:36:02 +0300

    my android project

--- app/src/main/java/com/example/androidproject/DisplayMessageActivity.java
+++ app/src/main/java/com/example/androidproject/DisplayMessageActivity.java
@@ -4,17 +4,28 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import static android.os.Build.VERSION_CODES.M;
 
@@ -92,5 +103,49 @@ public class DisplayMessageActivity extends AppCompatActivity {
             default:
                 return false;
         }
+    }
+
+    public void saveimage(View view){
+        saveReceivedImage("image");
+    }
+    private void saveReceivedImage(String imageName){
+        try {
+//            Bitmap bitmap = BitmapFactory.decodeByteArray(imageByteArray, 0, numberOfBytes);
+            Drawable drawable= ContextCompat.getDrawable(DisplayMessageActivity.this,R.drawable.imagehint);
+            Bitmap bitmap=Bitmap.createBitmap(drawable.getIntrinsicWidth(),drawable.getIntrinsicHeight(),Bitmap.Config.ARGB_8888);
+
+            File path = new File(DisplayMessageActivity.this.getFilesDir(), "AndroidProject" + File.separator + "Images");
+            if(!path.exists()){
+                path.mkdirs();
+            }
+            File outFile = new File(path, imageName + ".jpeg");
+            FileOutputStream outputStream = new FileOutputStream(outFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.close();
+            Toast.makeText(this, "Image saved on your internal storage", Toast.LENGTH_SHORT).show();
+
+        } catch (FileNotFoundException e) {
+            Log.e("msg", "Saving received message failed with", e);
+            Toast.makeText(this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void creatBitMap() {
+
+        File Med = new File(Environment.getExternalStorageDirectory(), "myDirName");
+        if (!Med.exists()) {
+            if (Med.mkdir()) {
+                Toast.makeText(this, "Directory Created", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.d("App", "Err");
+            }
+        }
+    }
+    public void storage(View view) {
+
+        Intent intent=new Intent(DisplayMessageActivity.this,InternalAndExternalStorage.class);
+        startActivity(intent);
     }
 }
